893272e7aa0dc5397398185f92877e6e45ac0744
Alex Giannoylis <alexgiannoulis3@gmail.com>
2023-02-03 12:20:31 +0200

    Clean the code and add address input resuse stats

--- app.js
+++ app.js
@@ -3,70 +3,63 @@ const app = express();
 const axios = require('axios');
 const fs = require('fs');
 const bitcore = require('bitcore-lib');
-
+let count = 0;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 async function getTransaction(transactionHash) {
-    try {
-      const response = await axios.get(`https://blockchain.info/rawtx/${transactionHash}`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await axios.get(`https://blockchain.info/rawtx/${transactionHash}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  async function readLines(filePath) {
-    return new Promise((resolve, reject) => {
-      let lines = [];
-      fs.createReadStream(filePath)
-        .on('data', (data) => {
-          let dataLines = data.toString().split("\n");
-          lines = lines.concat(dataLines);
-        })
-        .on('end', () => {
-          resolve(lines);
-        })
-        .on('error', (err) => {
-          reject(err);
-        });
-    });
-  }
+async function readLines(filePath) {
+  return new Promise((resolve, reject) => {
+    let lines = [];
+    fs.createReadStream(filePath)
+      .on('data', (data) => {
+        let dataLines = data.toString().split("\n");
+        lines = lines.concat(dataLines);
+      })
+      .on('end', () => {
+        resolve(lines);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+}
 
 app.get('/getTransactionInfo', async (req, res) => {
   let lines = await readLines("wasabi_txs_02-2022.txt");
-  var chosenLines = lines.slice(0, 5);
-  let errorCount = 0;
+  var chosenLine = lines[count];
+  count++;
   let transactionInfo = [];
   let addressToInputs = {};
   try {
-     for (let line of chosenLines) {
-        let info = await getTransaction(chosenLines[0]);
-        if (info) {
-            console.log("info aquired");
-            if (info?.inputs && info?.inputs?.length > 0) {
-                transactionInfo.push(info);
-                for (let input of info.inputs){
-                  console.log("checking previous transaction for input");
-                  let addr = input.prev_out.addr;
-                  if (!addressToInputs[addr]) {
-                    addressToInputs[addr] = []; 
-                  }
-                  addressToInputs[addr].push(input);
-                  console.log("Input sent from address " +addr);
-                }
-            } else {
-                console.log(" no block height");
-            }
+    let info = await getTransaction(chosenLine);
+    if (info) {
+      if (info?.inputs && info?.inputs?.length > 0) {
+        transactionInfo.push(info);
+        for (let input of info.inputs) {
+          let addr = input.prev_out.addr;
+          if (!addressToInputs[addr]) {
+            addressToInputs[addr] = [];
+          }
+          addressToInputs[addr].push(input);
         }
-     }
-    let addressesCount = Object.keys(addressToInputs).length;
-    res.json({transactionInfo, addressesCount});
+      } 
+    }
+    let addressesCount = Object.keys(addressToInputs)?.length ?? 0;
+    res.json({ transactionInfo, addressesCount });
   } catch (error) {
-    errorCount++;
+    res.json();
   }
 });
 
